90c076525e39fc1f365a8f161e2b6916fdd3e715
Conway Hogan <conway2019hogan@gmail.com>
2023-12-04 12:35:36 -0700

    created views and paths

--- index.js
+++ index.js
@@ -2,9 +2,25 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
+app.set('view-engine', 'ejs');
+
+app.get("/", (req, res) => { 
+    res.render('index.ejs')});
+
+app.get("/login", (req, res) => { 
+    res.render('login.ejs')});
+
+app.get("/register", (req, res) => { 
+    res.render('register.ejs')});
+
+app.get("/admin", (req, res) => { 
+    res.render('admin.ejs')});
+
+app.get("/dashboard", (req, res) => { 
+    res.render('dashboard.ejs')});
+
+app.get("/survey", (req, res) => { 
+    res.render('survey.ejs')});
 
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);

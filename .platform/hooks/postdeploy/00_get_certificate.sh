ec171002c2d7df74e752c87a583453b7f112d650
Conway Hogan <conway2019hogan@gmail.com>
2023-12-05 11:06:08 -0700

    fix

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,4 +1,4 @@
 
 #!/usr/bin/env bash
 # Place in .platform/hooks/postdeploy directory
-sudo certbot -n -d wellnessprovo.is404.net --nginx --agree-tos --email conway2019hogan@gmail.com
+sudo certbot -n -d wellnessprovo.is404.net  --nginx --agree-tos --email cjh242@byu.edu

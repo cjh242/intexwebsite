b5f49d555fb7c3009b208e147098ca1cf92617a9
Conway Hogan <conway2019hogan@gmail.com>
2023-12-04 20:31:53 -0700

    gitignore

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const path = require('path');
 const projectRoot = path.join('intex', '/');
 const bcrypt = require('bcrypt');
